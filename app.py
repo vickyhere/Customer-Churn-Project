64c2f56582f063408bcf5cec9dc3cb443bbcd57a
WAQAS HUSSAIN <waqashussain9075@gmail.com>
2024-09-19 03:41:55 +0500

    change whole project from mine to instructor

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-
 import streamlit as st
 import numpy as np
 import tensorflow as tf
-from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
 
+# Load the trained model
 model = tf.keras.models.load_model('model.h5')
 
-with open('one_hot_encoder_geo.pkl','rb') as file:
-    label_encoder_geo=pickle.load(file)
-
+# Load the encoders and scaler
 with open('label_encoder_gender.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
+
+with open('onehot_encoder_geo.pkl', 'rb') as file:
+    onehot_encoder_geo = pickle.load(file)
 
 with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
-st.title('Customer Churn Prediction')
+
+## streamlit app
+st.title('Customer Churn PRediction')
 
 # User input
-geography = st.selectbox('Geography', label_encoder_geo.categories_[0])
+geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
 balance = st.number_input('Balance')
@@ -30,7 +33,6 @@ tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
 is_active_member = st.selectbox('Is Active Member', [0, 1])
-
 
 # Prepare the input data
 input_data = pd.DataFrame({
@@ -46,8 +48,8 @@ input_data = pd.DataFrame({
 })
 
 # One-hot encode 'Geography'
-geo_encoded = label_encoder_geo.transform([[geography]]).toarray()
-geo_encoded_df = pd.DataFrame(geo_encoded, columns=label_encoder_geo.get_feature_names_out(['Geography']))
+geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
 
 # Combine one-hot encoded columns with input data
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
